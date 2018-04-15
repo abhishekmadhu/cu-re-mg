f64554c67b2a7fca8700d0c0328b7c2a3174e51f
Abhishek Madhu <21152008+abhishekmadhu@users.noreply.github.com>
2018-04-15 13:46:58 +0530

    Added productList to database

--- crm10/crm10/CustomerRegistrationForm.vb
+++ crm10/crm10/CustomerRegistrationForm.vb
@@ -6,4 +6,8 @@
     Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
 
     End Sub
+
+    Private Sub CustomerRegistrationForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
